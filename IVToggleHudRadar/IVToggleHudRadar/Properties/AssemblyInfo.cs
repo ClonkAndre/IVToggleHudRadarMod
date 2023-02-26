3354dd318d695460bde9d6a9b527cbe041919550
Elias <napaln@gmx.de>
2023-02-26 21:44:18 +0100

    Updated AssemblyInfo

--- IVToggleHudRadar/IVToggleHudRadar/Properties/AssemblyInfo.cs
+++ IVToggleHudRadar/IVToggleHudRadar/Properties/AssemblyInfo.cs
@@ -8,16 +8,16 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTitle("IVToggleHudRadar")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("ItsClonkAndre")]
 [assembly: AssemblyProduct("IVToggleHudRadar")]
-[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyCopyright("Copyright © ItsClonkAndre 2023")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible
 // to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
-[assembly: ComVisible(false)]
+[assembly: ComVisible(true)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
 [assembly: Guid("51be898d-226d-4e0c-9581-ccfd3418554e")]
